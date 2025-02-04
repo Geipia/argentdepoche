9ec578a6b0daccb2148e21a5faaea92ee21e141a
Geipia <peltieraloys@hotmail.com>
2025-02-04 18:55:47 +0100

    Ajout transactions

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 
-from app.models import Compte
+from app.models import Compte, Transaction
 
-admin.site.register(Compte)
+@admin.register(Compte)
+class CompteAdmin(admin.ModelAdmin):
+    list_display = ('name', 'salary', 'total')
+    search_fields = ['name']
+    list_filter = ['manager', 'client']
+    list_per_page = 10
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('compte', 'amount', 'description', 'created_at')
+    search_fields = ['compte']
+    list_filter = ['compte']
+    list_per_page = 10
 
 # Register your models here.
--- app/models.py
+++ app/models.py
@@ -5,11 +5,27 @@ class Compte(models.Model):
     # protected $fillable = ['user_id', 'manager_id', 'name', 'salary', 'total'];
     name = models.CharField(max_length=24)
     salary = models.FloatField(default=0)
-    total = models.FloatField(default=0)
+    #total = models.FloatField(default=0)
     manager = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comptes')
     client = models.ForeignKey(User, on_delete=models.CASCADE, related_name='mon_compte')
+    
+    @property
+    def total (self):
+        return self.transactions.aggregate(models.Sum('amount'))['amount__sum']
+
 
     def __str__(self):
         return self.name
+    
+
+class Transaction(models.Model):
+    # protected $fillable = ['compte_id', 'amount', 'type', 'description'];
+    compte = models.ForeignKey(Compte, on_delete=models.CASCADE, related_name='transactions')
+    amount = models.FloatField()
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.compte.name} - {self.amount}"
 
 # Create your models here.
